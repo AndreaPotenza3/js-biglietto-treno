2423d2e44c701985df556c6c46451f34973ac53a
AndreaPotenza <andreapoten3@gmail.com>
2024-10-15 14:45:50 +0200

    esercizio completo

---
+++ app.js
@@ -0,0 +1,28 @@
+const km = parseFloat(prompt("Quanti km è luungo il viaggio?"))
+const age = parseInt(prompt("Quanti anni hai?"))
+
+console.log(km)
+console.log(age)
+
+const price = km * 0.21
+console.log(price)
+
+let discountPercent
+
+if( age < 18) {
+    discountPercent = 20
+} else if (age > 65) {
+    discountPercent = 40
+} else {
+    discountPercent= 0
+}
+
+console.log("Percentuale sconto:", discountPercent)
+
+const discount = (price * discountPercent)
+console.log("sconto", discount)
+
+const finalPrice = price - discount
+console.log("prezzo", finalPrice.toFixed(2))
+
+
